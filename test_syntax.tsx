c1bf99332f33a5cb71e7aa21579d404c99eed820
Kasaewa <julia.kasaewa@gmail.com>
2025-12-22 20:06:37 +0100

    changes v2

---
+++ test_syntax.tsx
@@ -0,0 +1,17 @@
+// Test file to verify syntax
+const TestComponent = () => {
+  return (
+    <div>
+      {/* Comment */}
+      <section>
+        <div>Content</div>
+      </section>
+      {/* Another Comment */}
+      <section>
+        <div>More content</div>  
+      </section>
+    </div>
+  );
+};
+
+export default TestComponent;
